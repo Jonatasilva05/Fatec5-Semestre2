ef8df35ea42fa91e7bb6c1135ae5a24be5d815bf
JONATAS  DE MORAES DA SILVA <0220832311013@fatectq.local>
2025-08-18 09:45:56 -0300

    18-08-25

--- GIULIANO/java/soa_soap_calculo/src/webservice/SoapWebService.java
+++ GIULIANO/java/soa_soap_calculo/src/webservice/SoapWebService.java
@@ -1,8 +1,14 @@
 package webservice;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public class SoapWebService {
-    
+
+    @WebMethod
+    public double adicionar(@WebParam(name="valor1") double valor1, @WebParam(name="valor2") double valor2) {
+        return valor1 + valor2;
+    }
 }
