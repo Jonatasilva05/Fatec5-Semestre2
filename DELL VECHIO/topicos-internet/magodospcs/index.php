afa709175fe401106f538d8f2c6672628b15828c
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-08-29 12:19:20 -0300

    29-08

--- DELL VECHIO/topicos-internet/magodospcs/index.php
+++ DELL VECHIO/topicos-internet/magodospcs/index.php
@@ -3,27 +3,47 @@
     # Recebe todas as URI que vem do servidor, (requisições que vem do servidor), incluindo argumentos
     $uritotal = $_SERVER["REQUEST_URI"];
 
+
     # Função do PHP para dividir a uri em partes
     $partes = parse_url($uritotal);
 
-    echo "<pre>";
-    var_dump($partes);
-    echo "</pre>";
 
-    die(); # 'Mata' os testes, a partir daqui não haverá mais teste
+    # Recebe somente a parte da uri, basicamente um filtro evitando que os argumentos(query) apareça
+    $urirefinada = $partes["path"];
 
-    $uri = $_SERVER["REQUEST_URI"];
 
-    if($uri == "/"){
-        include __DIR__ . "/app/controllers/inicioController.php";
+    # Refina a uri retirando barras no inicio e no fim. Trim serve para retirar/aparar o selecionado
+    $urirefinada = trim($urirefinada, "/");
+
+
+    # Divide a uri a partir da barra, criando um array
+    $divisaouri = explode("/", $urirefinada);
+
+
+    # Pega somente o string na posição 0 do array
+    $uri = $divisaouri[0];
+
+
+    # Cria o array associativo de rotas
+    $rotas = [
+        "" => "inicioController", 
+        "quemsomos" => "quemsomosController",
+        "contato" => "contatoController"
+    ];
+
+
+    # Verifica se o uri existe como chave no array rotas
+    if(array_key_exists($uri, $rotas)){
+
+        # Define o controlador
+        $controlador = $rotas[$uri];
+
+        # Inclui o arquivo php do controlador
+        include __DIR__ . "/app/controllers/$controlador.php";
     }
-    else
-        if($uri == "/quemsomos"){
-        include __DIR__ . "/app/controllers/quemsomosController.php";
-    }
-    else
-        if($uri == "/contato"){
-        include __DIR__ . "/app/controllers/contatoController.php";
+    else{
+        # Se não encontrar a chave
+        echo "nao existe essa chave";
     }
 
 ?>
