242581326de1cd9a15a6fdccbd3117ff1cad7413
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-08-29 10:29:36 -0300

    29-08

--- DELL VECHIO/topicos-internet/magodospcs/index.php
+++ DELL VECHIO/topicos-internet/magodospcs/index.php
@@ -1,5 +1,17 @@
 <?php
 
+    # Recebe todas as URI que vem do servidor, (requisições que vem do servidor), incluindo argumentos
+    $uritotal = $_SERVER["REQUEST_URI"];
+
+    # Função do PHP para dividir a uri em partes
+    $partes = parse_url($uritotal);
+
+    echo "<pre>";
+    var_dump($partes);
+    echo "</pre>";
+
+    die(); # 'Mata' os testes, a partir daqui não haverá mais teste
+
     $uri = $_SERVER["REQUEST_URI"];
 
     if($uri == "/"){
