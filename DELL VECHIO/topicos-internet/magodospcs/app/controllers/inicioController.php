42196991771dd8f3fc24e78cf8277f7c4f9b83f9
Jonatasilva05 <jontasmoraes05@gmail.com>
2025-09-19 12:20:58 -0300

    19-09

--- DELL VECHIO/topicos-internet/magodospcs/app/controllers/inicioController.php
+++ DELL VECHIO/topicos-internet/magodospcs/app/controllers/inicioController.php
@@ -2,7 +2,28 @@
 
     $nomepagina = "MagodosPcs - Inicio";
 
-    $css = "../../css/estilos.css";
+    $css = [
+        "../../css/estilos.css",
+        "../../css/alternativo.css",
+    ];
+
+    # o for sempre sera 3 "categoria"
+    #primeira variavel sempre sera de controle e sempre recebera o valor de 0
+    #segunda variavel sera quando ele ira parar no nosso caso quando chegar em 2 ele para mas o 0 sempre sera menor entao ele nunca ira parar por isso temos a terceira variavel
+    #terceira variavel ira implementar mais um valor ao numero atual, ou seja sera a variavel $g que atualmente ela é igual a 0 entao depois disso ela sera somada com ++, no final ele sera 0+1, 1+1 e assim por diante ate ser 2 e parar no 2 que foi como configuramos ele
+    // for( $g=0; $g<2; $g++) {
+    //     echo $css[$g];
+    // }
+
+    #funciona igual mas de maneira mais curta
+    // $quantItens = count($css);
+    // for( $g=0; $g<$quantItens; $g++) {
+    //     echo $css[$g];
+    // }
+
+    for ( $g=0; $g<count($css); $g++) {
+        echo $css[$g];
+    }
 
 ?>
 
--- DELL VECHIO/topicos-internet/magodospcs/app/views/head.php
+++ DELL VECHIO/topicos-internet/magodospcs/app/views/head.php
@@ -2,5 +2,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $nomepagina ?></title>
-    <link rel="stylesheet" href="<?= $css ?>">
+    
+    <?php for($g=0; $g<count($css); $g++): ?>
+        <link rel="stylesheet" href="<?= $css[$g] ?>">
+    <?php endfor; ?>
+    
+    <!-- Esse é um dos metodos mas nao é totalmente necessario e tambem nem tanto ultilizado -->
+    <?php 
+
+        for($g=0; $g<count($css); $g++) {
+            #para nao colocar dessa maneira que talvez confunda 
+            // echo "<link rel='stylesheet' href='$css{[$g]}'>";
+
+            // faça dessa maneira
+            $posicao = $css[$g];
+            echo "<link rel='stylesheet' href='$posicao'>";
+        }
+
+    ?>
 </head>
